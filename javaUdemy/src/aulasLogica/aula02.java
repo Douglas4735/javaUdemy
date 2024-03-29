7aee03b3b5e4394e36480a0b457b4b7e7523c8c3
douglas4735 <douglas4735@gmail.com>
2024-03-28 22:32:42 -0400

    primeiro commit

---
+++ javaUdemy/src/aulasLogica/aula02.java
@@ -0,0 +1,21 @@
+package aulasLogica;
+
+import java.util.Scanner;
+
+public class aula02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		while(x != 0) {
+			x = sc.nextInt();
+		}
+		System.out.println("FIM");
+		
+		sc.close();
+	}
+
+}
