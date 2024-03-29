1105d36e08a0c04e00cb24637430e40b5b81fce5
douglas4735 <douglas4735@gmail.com>
2024-03-29 13:06:40 -0400

    rename aula02 para aula02while

--- javaUdemy/src/aulasLogica/aula02while.java
+++ javaUdemy/src/aulasLogica/aula02while.java
@@ -2,19 +2,21 @@ package aulasLogica;
 
 import java.util.Scanner;
 
-public class aula02 {
+public class aula02while {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
+		int soma = 0;
 		int x = sc.nextInt();
 		
 		while(x != 0) {
+			soma += x; /*ou soma = soma + x;*/
 			x = sc.nextInt();
 		}
 		
-		System.out.println("FIM DA EXECUÇÃO");
+		System.out.println("soma dos numeros digitados são = " +soma);
 		
 		sc.close();
 	}
